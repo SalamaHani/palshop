93903634f310dd31a85252ca9d9239b0e8367378
Salama <122737355+SalamaHani@users.noreply.github.com>
2025-12-30 17:21:42 +0200

    efreffvf;.sr

--- hooks/useStorefront.ts
+++ hooks/useStorefront.ts
@@ -34,9 +34,9 @@ export function useStorefrontMutation<
   TVariables extends MutationVariables = MutationVariables
 >() {
   const mutation = useMutation<TData, Error, TVariables>({
-    mutationFn: async ({ query, variables, endpointType = "storefront" }) => {
+    mutationFn: async ({ query, variables }) => {
       try {
-        const response = await fetchShopify<TData>(query, variables, endpointType);
+        const response = await fetchShopify<TData>(query, variables);
         return response;
       } catch (error) {
         // Type guard to ensure error is an Error object
